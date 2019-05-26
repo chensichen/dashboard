b03f6d9922cd1dc465ec842fad83fa809ef289fb
chensh <605071711@qq.com>
2019-05-26 16:47:08 +0800

    [chensh] #n/a 添加dashboard

---
+++ dashboard/src/components/LeftNav.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="left">
+    <ul>
+      <li
+        v-for="(item, index) in nav"
+        :key="index"
+        :class="{ activeClass: index == activeNum }"
+        @click="changeRouter(index)"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "LeftNav",
+  data() {
+    return {
+      activeNum: 0,
+      nav: [
+        { name: "张三", id: 1 },
+        { name: "李四", id: 2 },
+        { name: "王五", id: 3 },
+        { name: "赵柳", id: 4 }
+      ]
+    };
+  },
+  methods: {
+    changeRouter(index) {
+      this.activeNum = index;
+      // this.$router.push("flex" + (index + 1));
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.left {
+  display: flex;
+  width: 200px;
+  height: 100%;
+  border: 1px solid red;
+  ul {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    li {
+      padding: 10px 10px;
+      cursor: pointer;
+    }
+    .activeClass {
+      background: #ccc;
+      position: relative;
+    }
+    .activeClass::after {
+      position: absolute;
+      height: 50%;
+      left: 0%;
+      top: 10px;
+      display: block;
+      border: 3px solid blue;
+      content: "";
+    }
+  }
+}
+</style>
---
+++ dashboard/src/views/flex4.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    flex4
+  </div>
+</template>
+<script>
+export default {
+  name: "flex4",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="less" scoped></style>
